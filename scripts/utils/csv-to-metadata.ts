3b2c559d0ae4be164c4dd8744d71ea5e27288d50
ccarrasco <ccarrasco@leverade.com>
2022-02-10 10:48:17 +0100

    I keep developing the scaffolding for the script

--- scripts/utils/csv-to-metadata.ts
+++ scripts/utils/csv-to-metadata.ts
@@ -12,14 +12,12 @@ const rl = readline.createInterface({
 });
 
 // TODO: Be more clear about what the input path means (is it relative? Must it be inside a specific folder?)
+// TODO: Assert input path exists
+// TODO: Validate input path as valid CSV
 rl.question(
   "Please, write the relative path of the CSV file: ",
   function (inputPath) {
     rl.close();
-    // rl.on("close", function () {
-    //   console.log("\nBYE BYE !!!");
-    //   process.exit(0);
-    // });
 
     console.log(`The introduced file is ${inputPath}`);
     console.log(`The output file will be ${outputPath}`);
@@ -27,34 +25,37 @@ rl.question(
     if (!fs.existsSync(outputSubPath)) {
       fs.mkdirSync(outputSubPath, { recursive: true });
     }
-    fs.writeFileSync(outputPath, "Hey there!");
+    const jsonObject = toJsonObject();
+    fs.writeFileSync(outputPath, JSON.stringify(jsonObject));
   }
 );
 
-// const expected = {
-//   description:
-//     "Colección conmemorativa de las medallistas de la temporada 2021/2022 de la Copa de la Reina de Waterpolo de España, acuñada por la Real Federación Española de Natación.",
-//   name: "Copa de la Reina de Waterpolo 2021/2022 #1",
-//   image: "https://static.leverade.com/img/brand_logo.svg",
-//   animation_url: "https://static.leverade.com/img/brand_logo.gif",
-//   background_color: "ffcd00",
-//   external_url: "https://rfen.es/tournament/1136595/",
-//   attributes: [
-//     {
-//       trait_type: "Athlete",
-//       trait_value: "Nombre Apellidos",
-//     },
-//     {
-//       trait_type: "Club",
-//       trait_value: "C.N. Catalunya",
-//     },
-//     {
-//       trait_type: "Medal",
-//       trait_value: "Gold",
-//     },
-//     {
-//       trait_type: "Sponsor",
-//       trait_value: "Iberdrola",
-//     },
-//   ],
-// };
+function toJsonObject(): object {
+  return {
+    description:
+      "Colección conmemorativa de las medallistas de la temporada 2021/2022 de la Copa de la Reina de Waterpolo de España, acuñada por la Real Federación Española de Natación.",
+    name: "Copa de la Reina de Waterpolo 2021/2022 #1",
+    image: "https://static.leverade.com/img/brand_logo.svg",
+    animation_url: "https://static.leverade.com/img/brand_logo.gif",
+    background_color: "ffcd00",
+    external_url: "https://rfen.es/tournament/1136595/",
+    attributes: [
+      {
+        trait_type: "Athlete",
+        trait_value: "Nombre Apellidos",
+      },
+      {
+        trait_type: "Club",
+        trait_value: "C.N. Catalunya",
+      },
+      {
+        trait_type: "Medal",
+        trait_value: "Gold",
+      },
+      {
+        trait_type: "Sponsor",
+        trait_value: "Iberdrola",
+      },
+    ],
+  };
+}
