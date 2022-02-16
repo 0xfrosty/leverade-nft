48219e786fbb6954cbda6ee897221c7c38710bc5
ccarrascoml <40393675+ccarrascoml@users.noreply.github.com>
2022-02-16 18:05:38 +0100

    LV-3406 Scripts to generate metadata from CSV file (#1)
    * First iteration of script to generate NFT metadata from CSV file
    * First iteration of script to generate JSON frontend file from CSV file

---
+++ scripts/utils/csv-to-leverade.ts
@@ -0,0 +1,99 @@
+import * as fs from "fs";
+import { join } from "path";
+import * as readline from "readline";
+import { parse } from "csv-parse";
+import * as lodash from "lodash";
+
+// TODO: Make this path configurable by the user
+const outputSubPath = join(".", "public", "leverade-output");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// TODO: Be more clear about what the input path means (is it relative? Must it be inside a specific folder?)
+// TODO: Assert input path exists
+// TODO: Validate input path as valid CSV
+rl.question(
+  "Please, write the relative path of the CSV file: ",
+  function (inputPath) {
+    rl.close();
+
+    if (!fs.existsSync(inputPath)) {
+      console.error("The introduced file path does not exist. Exiting...");
+      process.exitCode = 1;
+      return;
+    }
+
+    const rawCsvInfo: string = fs.readFileSync(inputPath, "utf-8");
+    parse(
+      rawCsvInfo,
+      {
+        delimiter: ",",
+      },
+      function (err, records) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        console.log("Assuring output folder exists...");
+        if (!fs.existsSync(outputSubPath)) {
+          fs.mkdirSync(outputSubPath, { recursive: true });
+        }
+
+        console.log("Building metadata JSON files...");
+        const nfts: any[] = [];
+        for (const key in records) {
+          if (key === "0") continue;
+          console.log(`- Generating metadata JSON file for row ${key}...`);
+          const csvObject: any = csvToObject(records[key], records[0]);
+          nfts.push(toJsonObject(csvObject));
+        }
+        const outputPath = join(outputSubPath, "output.json");
+        fs.writeFileSync(outputPath, JSON.stringify(nfts));
+      }
+    );
+  }
+);
+
+function csvToObject(record: object, headers: object): object {
+  const headersValues = Object.values(headers);
+  const recordValues = Object.values(record);
+
+  const output: any = {};
+
+  headersValues.forEach((value, key) => {
+    lodash.set(output, value, recordValues[key]);
+  });
+
+  return output;
+}
+
+function toJsonObject(csvObject: any): object {
+  const athleteTrait = csvObject.attributes.find(function (o: any) {
+    return o.trait_type === "Athlete";
+  });
+
+  const clubTrait = csvObject.attributes.find(function (o: any) {
+    return o.trait_type === "Club";
+  });
+
+  const awardTrait = csvObject.attributes.find(function (o: any) {
+    return o.trait_type === "Award";
+  });
+
+  return {
+    id: csvObject.nft_id,
+    profileId: csvObject.profile_id,
+    nftName: csvObject.name,
+    athleteName: athleteTrait.value,
+    image: csvObject.image,
+    club: clubTrait.value,
+    clubUrl: csvObject.club_url,
+    type: awardTrait.value.toLowerCase().replace(" ", "_"),
+    competitionUrl: csvObject.competition_url,
+    openseaUrl: `https://opensea.io/assets/matic/0xfb917e76927e2bed4361baa088c6e680e8546c4b/${csvObject.nft_id}`,
+  };
+}
